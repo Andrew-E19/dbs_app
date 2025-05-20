e0ea44a5d41c42e09fa05779d6cdd0ae2371f098
Andrew-E19 <louiegut19@gmail.com>
2025-05-20 16:52:28 +0800

    updated code

--- classes/database.php
+++ classes/database.php
@@ -2,7 +2,7 @@
 
 class database {
 
-    function opencon(): PDO {
+    function opencon(){
         return new PDO(
             dsn: 'mysql:host=localhost;dbname=dbs_app',
             username: 'root',
@@ -10,14 +10,14 @@ class database {
         );
     }
 
-    function signupUser($firstname, $lastname, $username, $password) {
+    function signupUser($firstname, $lastname, $username, $email, $password) {
         $con = $this->opencon();
 
         try {
             $con->beginTransaction();
 
-            $stmt = $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$firstname, $lastname, $username, $password]);
+            $stmt = $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, admin_username, admin_email, admin_password) VALUES (?, ?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $username, $email, $password]);
             $userID = $con->lastInsertId();
             $con->commit();
 
@@ -32,4 +32,36 @@ class database {
 
     }
 
+    function isUsernameExists($username){
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT COUNT(*) FROM Admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $count = $stmt->fetchColumn();
+
+        return $count > 0;
+    }
+
+    function isEmailExists($email){
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT COUNT(*) FROM Admin WHERE admin_email = ?");
+        $stmt->execute([$email]);
+        $count = $stmt->fetchColumn();
+
+        return $count > 0;
+    }
+
+    function loginUser($username, $password) {
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT * FROM Admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($user && password_verify($password, $user['admin_password'])) {
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
 }
+
+?>
